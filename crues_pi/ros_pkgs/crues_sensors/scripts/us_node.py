b2c2c0a74e60e7544049afd3580d60a99d328b1f
R. David Dunphy <rdd.dunphy@gmail.com>
2019-02-23 00:21:32 +0000

    Support multiple ultrasonic sensors.

--- crues_pi/ros_pkgs/crues_sensors/scripts/us_node.py
+++ crues_pi/ros_pkgs/crues_sensors/scripts/us_node.py
@@ -1,49 +1,75 @@
 #!/usr/bin/env python
 
+import time
+
 import rospy
 from std_msgs.msg import Int32
 
 from crues import us, pin_defs
 
 
+## VERSION FOR ONLY CENTRE ULTRASONIC SENSOR:
+# def main():
+#     try:
+#         us.configure_gpio()
+#         pub = rospy.Publisher('uc_range', Int32, queue_size=10)
+#         rospy.init_node("uc")
+#         rate = rospy.Rate(10)
+#         while not rospy.is_shutdown():
+#             try:
+#                 rospy.loginfo("Trying to get ultrasonic range from uc")
+#                 range = us.get_range(us.CENTRE)
+#             except us.UltrasonicTimout as e:
+#                 rospy.logwarn(str(e))
+#             else:
+#                 rospy.loginfo("Range from uc: %d", range)
+#                 pub.publish(range)
+#             rate.sleep()
+#         rospy.warn("uc thinks rospy.is_shutdown???")
+#     except rospy.ROSInterruptException as e:
+#         rospy.logerr("ROSInterruptException in node uc")
+#         rospy.logerr(e)
+#     finally:
+#         pin_defs.cleanup()
+
+
+def publish_range(s, pub):
+    try:
+        r = us.get_range(s)
+    except us.UltrasonicTimout as e:
+        rospy.logwarn(str(e))
+    else:
+        rospy.loginfo("%s ultrasonic node range: %d", (us.sensor_str.get(s), r))
+        pub.publish(r)
+
+
 def main():
     try:
         us.configure_gpio()
-        pub = rospy.Publisher('uc_range', Int32, queue_size=10)
-        rospy.init_node("uc")
-        rate = rospy.Rate(10)
+        pub_l = rospy.Publisher('ul_range', Int32, queue_size=10)
+        pub_c = rospy.Publisher('uc_range', Int32, queue_size=10)
+        pub_r = rospy.Publisher('ur_range', Int32, queue_size=10)
+        rospy.init_node("ultrasonic")
+        rate = rospy.Rate(5)
         while not rospy.is_shutdown():
-            try:
-                rospy.loginfo("Trying to get ultrasonic range from uc")
-                range = us.get_range(us.CENTRE)
-            except us.UltrasonicTimout as e:
-                rospy.logwarn(str(e))
-            else:
-                rospy.loginfo("Range from uc: %d", range)
-                pub.publish(range)
+            start = time.time()
+            publish_range(us.LEFT, pub_l)
+            time_remaining = start + us.scan_increment - time.time()
+            if time_remaining > 0:
+                time.sleep(time_remaining)
+            publish_range(us.CENTRE, pub_c)
+            time_remaining = start + 2 * us.scan_increment - time.time()
+            if time_remaining > 0:
+                time.sleep(time_remaining)
+            publish_range(us.RIGHT, pub_r)
+            # Additionally publish scan object for SLAM node?
             rate.sleep()
-        rospy.warn("uc thinks rospy.is_shutdown???")
+        rospy.logwarn("ultrasonic node thinks rospy.is_shutdown???")
     except rospy.ROSInterruptException as e:
-        rospy.logerr("ROSInterruptException in node uc")
+        rospy.logerr("ROSInterruptException in ultrasonic node")
         rospy.logerr(e)
     finally:
         pin_defs.cleanup()
-
-#
-# def get_args():
-#     parser = argparse.ArgumentParser()
-#     parser.add_argument("--sid", type=str, default="centre")
-#     args = parser.parse_args()
-#     if (args.sid == "left"):
-#         sensor = us.LEFT
-#         node_name = "ul"
-#     elif (args.sid == "right"):
-#         sensor = us.RIGHT
-#         node_name = "ur"
-#     else:
-#         sensor = us.CENTRE
-#         node_name = "uc"
-#     return sensor, node_name
 
 
 if __name__ == '__main__':
