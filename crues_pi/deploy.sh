756545b4fa5a3e7a9fc8fa52ad554ed3c09059cc
R. David Dunphy <rdd.dunphy@gmail.com>
2019-03-08 11:29:06 +0000

    Make deploy script talk to you.

--- crues_pi/deploy.sh
+++ crues_pi/deploy.sh
@@ -7,7 +7,7 @@
 
 if [[ $# -eq 0 ]]
 then
-    echo "Enter robot names as arguments."
+    echo "Enter robot names as arguments"
 else
     for NAME in "$@"
     do
@@ -21,14 +21,17 @@ else
         then
             IP=192.168.1.4
         else
-            echo "That's not a valid robot name"
+            echo "${NAME} is not a valid robot name"
             exit
         fi
-        echo "Deploying to ${NAME} (crues@${IP})..."
-        sshpass -p "dapamaka" rsync -r crues/ crues@${IP}:~/crues_pi/crues/
-        sshpass -p "dapamaka" rsync setup.py crues@${IP}:~/crues_pi/
-        sshpass -p "dapamaka" rsync requirements.txt crues@${IP}:~/crues_pi/
-        sshpass -p "dapamaka" rsync -r ros_pkgs/ crues@${IP}:~/catkin_ws/src/
-        sshpass -p "dapamaka" rsync -r config/ crues@${IP}:~/crues_pi/config/
+        echo "Deploying to ${NAME} (crues@${IP})"
+        echo "  - Library files..."
+        sshpass -p "dapamaka" rsync --stats -r crues/ crues@${IP}:~/crues_pi/crues/ | grep 'files transferred'
+        sshpass -p "dapamaka" rsync --stats setup.py crues@${IP}:~/crues_pi/ | grep 'files transferred'
+        sshpass -p "dapamaka" rsync --stats requirements.txt crues@${IP}:~/crues_pi/ | grep 'files transferred'
+        echo "  - Configuration files..."
+        sshpass -p "dapamaka" rsync --stats -r config/ crues@${IP}:~/crues_pi/config/ | grep 'files transferred'
+        echo "  - ROS packages..."
+        sshpass -p "dapamaka" rsync --stats  -r ros_pkgs/ crues@${IP}:~/catkin_ws/src/ | grep 'files transferred'
     done
 fi
