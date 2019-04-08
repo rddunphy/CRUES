04d2bf5a2e03ec16015a817a13ad26b85ee50ab2
R. David Dunphy <rdd.dunphy@gmail.com>
2019-04-08 23:38:58 +0100

    Add errors to GPIO_MOCK for debugging

--- crues_pi/crues/GPIO_MOCK.py
+++ crues_pi/crues/GPIO_MOCK.py
@@ -9,20 +9,31 @@ RISING = 1
 BOTH = 2
 PUD_DOWN = 0
 
+_output_pins = []
+_input_pins = []
+
+
+class GPIO_ERROR(Exception):
+    pass
+
 
 def setmode(mode):
     print("Setting GPIO mode to %d" % mode)
 
 
-def setup(pins, mode, initial=LOW, pull_up_down=PUD_DOWN):
-    if isinstance(pins, list):
-        for pin in pins:
-            setup(pin, mode, initial=initial, pull_up_down=pull_up_down)
+def setup(pin, mode, initial=LOW, pull_up_down=PUD_DOWN):
+    if isinstance(pin, list):
+        for p in pin:
+            setup(p, mode, initial=initial, pull_up_down=pull_up_down)
     else:
+        if pin in _input_pins or pin in _output_pins:
+            raise GPIO_ERROR("Multiple pin definitions for %d" % pin)
         if mode == OUT:
-            print("Setting pin %d as output with initial value %d" % (pins, initial))
+            _output_pins.append(pin)
+            print("Setting pin %d as output with initial value %d" % (pin, initial))
         else:
-            print("Setting pin %d as input with pull-up/down %d" % (pins, pull_up_down))
+            _input_pins.append(pin)
+            print("Setting pin %d as input with pull-up/down %d" % (pin, pull_up_down))
 
 
 def add_event_detect(pin, edge, callback=None):
@@ -31,7 +42,7 @@ def add_event_detect(pin, edge, callback=None):
         edge_str = "rising edge"
     elif edge == FALLING:
         edge_str = "falling edge"
-    if not callback is None:
+    if callback is not None:
         print("Registering %s for %s on pin %d" % callback.__name__, edge_str, pin)
 
 
@@ -44,12 +55,14 @@ def output(pin, signal):
     print("Writing %d to pin %d" % (signal, pin))
 
 
-def cleanup(pins):
-    if isinstance(pins, list):
-        for pin in pins:
-            cleanup(pin)
+def cleanup(pin):
+    if isinstance(pin, list):
+        for p in pin:
+            cleanup(p)
     else:
-        print("Cleaning up %d" % pins)
+        if pin not in _input_pins and pin not in _output_pins:
+            raise GPIO_ERROR("Pin %d not yet defined" % pin)
+        print("Cleaning up %d" % pin)
 
 
 class PWM:
